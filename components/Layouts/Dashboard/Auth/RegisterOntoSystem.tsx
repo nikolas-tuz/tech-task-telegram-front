c35a3d253437eb8f0cba75e4f84b63fa861f032d
nikolas-tuz <tuznikolas@gmail.com>
2025-04-25 08:35:53 +0300

    Implemented register form validation using zod;

--- components/Layouts/Dashboard/Auth/RegisterOntoSystem.tsx
+++ components/Layouts/Dashboard/Auth/RegisterOntoSystem.tsx
@@ -1,4 +1,4 @@
-// 'use client';
+'use client';
 
 import { AuthStateType } from '@/components/Pages/Home/Auth';
 import DivContainer from '@/components/UI/DivContainer';
@@ -7,7 +7,9 @@ import TextNeutral from '@/components/Typography/Text/TextNeutral';
 import Input from '@/components/Inputs/Input';
 import Button from '@/components/Buttons/Button';
 import TextGreen from '@/components/Typography/Text/TextGreen';
-import { ComponentPropsWithoutRef } from 'react';
+import { ComponentPropsWithoutRef, FormEvent, useState } from 'react';
+import { registerSchema } from '@/utils/schemas/register.schema';
+import ErrorMessage from '@/components/Layouts/Info/ErrorMessage';
 
 type RegisterOntoSystemType = {
   setAuthState: (state: AuthStateType) => void;
@@ -15,15 +17,43 @@ type RegisterOntoSystemType = {
 } & ComponentPropsWithoutRef<'div'>;
 
 export default function RegisterOntoSystem({ className, setAuthState, ...props }: RegisterOntoSystemType) {
+  const [errorMessage, setErrorMessage] = useState(``);
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const currObject = e.currentTarget;
+    const formData = new FormData(currObject);
+    const results = Object.fromEntries(formData.entries()) as {
+      email: string;
+      password: string;
+      confirmPassword: string
+    };
+
+    const validate = registerSchema.safeParse({
+      email: results.email, password: results.password,
+      confirmPassword: results.confirmPassword
+    });
+
+    if (!validate.success) {
+      setErrorMessage(validate.error.errors[0].message);
+      return;
+    }
+  }
+
   return (
-    <>
+    <form onSubmit={handleSubmit}>
       <DivContainer {...props} className={`text-center flex flex-col items-center justify-center ${className}`}>
         <LogoIcon className={`h-24 w-40 mb-8`} />
         <TextNeutral className={`mb-4`}>Register Onto System</TextNeutral>
         <DivContainer className={`w-full flex flex-col gap-3 mb-14`}>
-          <Input type={`email`} placeholder={`Email`} />
-          <Input type={`password`} placeholder={`Password`} />
-          <Input type={`password`} placeholder={`Confirm Password`} />
+          <Input onFocus={() => setErrorMessage(``)} invalid={!!errorMessage} name={`email`} type={`email`}
+                 placeholder={`Email`} />
+          <Input onFocus={() => setErrorMessage(``)} invalid={!!errorMessage} name={`password`} type={`password`}
+                 placeholder={`Password`} />
+          <Input onFocus={() => setErrorMessage(``)} invalid={!!errorMessage} name={`confirmPassword`} type={`password`}
+                 placeholder={`Confirm Password`} />
+
+          {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
         </DivContainer>
         <DivContainer className={`w-full flex flex-col gap-3`}>
           <Button className={`w-full`}>Register</Button>
@@ -34,6 +64,6 @@ export default function RegisterOntoSystem({ className, setAuthState, ...props }
           </button>
         </DivContainer>
       </DivContainer>
-    </>
+    </form>
   );
 }
--- utils/schemas/register.schema.ts
+++ utils/schemas/register.schema.ts
@@ -2,8 +2,8 @@ import { z } from 'zod';
 
 export const registerSchema = z.object({
   email: z.string({ message: `Please enter a valid email!` }).trim().email(`Please enter a valid email!`).nonempty(),
-  password: z.string({ message: `Please enter valid password!` }).trim().min(5).max(3000),
+  password: z.string({ message: `Please enter valid password!` }).trim().min(5, `Password should contain at least 5 characters.`).max(3000),
   confirmPassword: z.string({ message: `Please enter valid password!` }).trim().nonempty(`Confirm Password should not be empty.`)
 }).refine(({ password, confirmPassword }) => {
   return password.length >= 5 && password === confirmPassword;
-});
+}, { message: `Passwords do not match.` });
